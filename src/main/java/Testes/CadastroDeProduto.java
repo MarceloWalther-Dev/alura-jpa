0092f7e984b3fe29780faf824f258ccf026c8d0b
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2022-04-25 07:38:33 -0300

    Estados no insert da entidade

--- src/main/java/Testes/CadastroDeProduto.java
+++ src/main/java/Testes/CadastroDeProduto.java
@@ -1,29 +1,31 @@
 package Testes;
 
+import dao.CategoriaDao;
 import dao.ProdutoDao;
+import domain.model.Categoria;
 import domain.model.Produto;
-import enuns.Categoria;
 import util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
 
-        Produto celular = new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"), Categoria.CELULARES);
+        Categoria categoriaCelular = new Categoria("Celular");
+        Produto celular = new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"),categoriaCelular );
 
 
         EntityManager em = JPAUtil.getEntityManager();
-
         ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
 
         em.getTransaction().begin();
 
-
+        categoriaDao.cadastrar(categoriaCelular);
         produtoDao.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
 
